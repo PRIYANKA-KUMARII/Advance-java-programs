2b680437f038bfc88a513c07904fc811658e08a1
Priyanka Kumari <kumaripriyanka94528@gmail.com>
2025-05-27 19:09:45 +0530

    Login Page Of Bank

---
+++ Login.java
@@ -0,0 +1,23 @@
+package com.priya;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+public class Login extends JFrame implements ActionListener {
+//	JLabel 11, 12, 13;
+	JTextField tf1;
+	JPasswordField pf2;
+	JButton b1, b2, b3;
+	Login(){
+		setTitle("AUTOMATED TELLER MACHINE");
+		ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("https://www.google.co.in/url?sa=i&url=https%3A%2F%2Fwww.freeiconspng.com%2Fimg%2F5964&psig=AOvVaw0Vr8m5hMTyclr4J3iLv8cl&ust=1745859683672000&source=images&cd=vfe&opi=89978449&ved=0CBcQjhxqFwoTCKigiKPY-IwDFQAAAAAdAAAAABAE");
+	    Image i2= i1.getImage().getScaledInstance(100, 100 Image.scale_default);
+	    Imageicon i3= new ImageIcon(i2);
+	    JLabel 111= new JLabel(i3);
+	
+	}
+	 
+	}
+	
+
+}
